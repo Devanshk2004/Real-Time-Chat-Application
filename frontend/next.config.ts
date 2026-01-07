1586b393bb736f7a4bac0efa30971f39bbfe7d04
Devanshk2004 <devansh.kalura55@gmail.com>
2026-01-07 21:28:09 +0530

    deployment

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",      // Exports static HTML/CSS/JS
+  distDir: "dist",       // Changes output folder from .next to dist
+  images: {
+    unoptimized: true,   // Required for static export
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
